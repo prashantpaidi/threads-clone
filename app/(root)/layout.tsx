329ef7240c213bf01639f1c818dda8258ee1c150
prashantpaidi <88718449+prashantpaidi@users.noreply.github.com>
2023-08-18 23:34:43 +0530

    feat: create HTML and CSS for Main Page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,12 @@
 import '../globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Bottombar from '@/components/shared/Bottombar';
+import RightSidebar from '@/components/shared/RightSidebar';
+import LeftSidebar from '@/components/shared/LeftSidebar';
+import Topbar from '@/components/shared/Topbar';
+import { ClerkProvider } from '@clerk/nextjs';
+// import { dark } from '@clerk/themes';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,8 +21,29 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider
+      appearance={
+        {
+          // baseTheme: dark,
+        }
+      }
+    >
+      <html lang='en'>
+        <body className={inter.className}>
+          <Topbar />
+
+          <main className='flex flex-row'>
+            <LeftSidebar />
+            <section className='flex min-h-screen flex-1 flex-col items-center bg-dark-1 px-6 pb-10 pt-28 max-md:pb-32 sm:px-10'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            {/* @ts-ignore */}
+            <RightSidebar />
+          </main>
+
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
