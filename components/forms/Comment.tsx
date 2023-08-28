228e1cbfdf8357dc39ad78b5b96a0763e5b14f28
prashantpaidi <88718449+prashantpaidi@users.noreply.github.com>
2023-08-28 18:49:29 +0530

    add: Enable Posting of Text Content

---
+++ components/forms/Comment.tsx
@@ -0,0 +1,93 @@
+'use client';
+
+import { z } from 'zod';
+import Image from 'next/image';
+import { useForm } from 'react-hook-form';
+import { usePathname } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from '@/components/ui/form';
+
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
+
+import { CommentValidation } from '@/lib/validations/thread';
+import { addCommentToThread } from '@/lib/actions/thread.actions';
+
+interface Props {
+  threadId: string;
+  currentUserImg: string;
+  currentUserId: string;
+}
+
+function Comment({ threadId, currentUserImg, currentUserId }: Props) {
+  const pathname = usePathname();
+
+  const form = useForm<z.infer<typeof CommentValidation>>({
+    resolver: zodResolver(CommentValidation),
+    defaultValues: {
+      thread: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof CommentValidation>) => {
+    await addCommentToThread(
+      threadId,
+      values.thread,
+      JSON.parse(currentUserId),
+      pathname
+    );
+
+    form.reset();
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        className='m-10 flex items-center gap-4 border-y border-y-dark-4 py-5 max-xs:flex-col'
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <FormField
+          control={form.control}
+          name='thread'
+          render={({ field }) => (
+            <FormItem className='flex w-full items-center gap-3'>
+              <FormLabel>
+                <Image
+                  src={currentUserImg}
+                  alt='current_user'
+                  width={48}
+                  height={48}
+                  className='rounded-full object-cover'
+                />
+              </FormLabel>
+              <FormControl className='border-none bg-transparent'>
+                <Input
+                  type='text'
+                  {...field}
+                  placeholder='Comment...'
+                  className='no-focus text-light-1 outline-none'
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <Button
+          type='submit'
+          className='rounded-3xl bg-primary-500 px-8 py-2 !text-small-regular text-light-1 max-xs:w-full'
+        >
+          Reply
+        </Button>
+      </form>
+    </Form>
+  );
+}
+
+export default Comment;
